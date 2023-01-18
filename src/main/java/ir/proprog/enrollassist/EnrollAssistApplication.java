e7785e3a943d97d40e283b5fc5dfde89349b8ffe
Seyed Mohammad Amin Atyabi <seyedamin1990@gmail.com>
2023-01-18 22:08:19 +0330

    Force locale to english

--- src/main/java/ir/proprog/enrollassist/EnrollAssistApplication.java
+++ src/main/java/ir/proprog/enrollassist/EnrollAssistApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class EnrollAssistApplication {
 
     public static void main(String[] args) {
+        // Software is not working on systems with local any other than english
+        Locale.setDefault(Locale.ENGLISH);
         SpringApplication.run(EnrollAssistApplication.class, args);
     }
 
